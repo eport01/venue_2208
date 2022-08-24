9b36ef665018c0c8ac4e9b8bc514fc96d7e4fb9d
Emily Port <emilylport@gmail.com>
2022-08-24 09:58:50 -0600

    pass iteration 3 tests

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,14 +1,16 @@
 class Venue
-  attr_reader :name, :venue, :patrons, :capacity  
+  attr_reader :name, :venue, :patrons, :capacity, :patron_count  
   def initialize(name, venue)
     @name = name
     @venue = venue
     @capacity = 4  
     @patrons = []
+    @patron_count = 0
     @yell_at_patrons = []
   end 
 
   def add_patron(patron_name)
+    @patron_count += 1
     @patrons << patron_name
   end 
 
@@ -17,4 +19,12 @@ class Venue
       @yell_at_patrons << patron.upcase! 
     end 
   end 
+
+  def over_capacity?
+    if patron_count >= 4
+      true 
+    else
+      false 
+    end  
+  end     
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -51,4 +51,32 @@ describe Venue do
       expect(venue.yell_at_patrons).to eq ['MIKE', 'MEGAN', 'BOB']
     end
   end
+
+
+  #Iteration 3
+
+  describe 'over capacity' do 
+    it 'will return true or false venue has more patrons than capacity' do 
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+
+      expect(venue.over_capacity?).to eq false  
+
+      venue.add_patron('James')
+      venue.add_patron('Cat')
+
+      expect(venue.over_capacity?).to eq true  
+    end 
+  end 
+
+
+
+
+
 end
+
+
+
+
